77727da970f0bc272172e8074cf1d729bf12e4d6
lennertamas <tamas.lenner@gmail.com>
2024-06-20 10:54:28 +0200

    test repaired

--- test_bl.py
+++ test_bl.py
@@ -2,11 +2,35 @@ import bl
 import pytest
 import allure
 
+@allure.title("Test 4 is not prime ")
+@allure.description("This test attempts to test wether 4 is prime or not.")
+@allure.tag("NewUI", "Essentials", "Authentication")
+@allure.label("owner", "John Doe")
+@allure.link("https://dev.example.com/", name="Website")
+@allure.issue("AUTH-123")
+@allure.testcase("TMS-456")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_4_is_prime():
     assert bl.is_prime(4) == False
 
+@allure.title("Test 5 is prime ")
+@allure.description("This test attempts to test wether 4 is prime or not.")
+@allure.tag("NewUI", "Essentials", "Authentication")
+@allure.label("owner", "John Doe")
+@allure.link("https://dev.example.com/", name="Website")
+@allure.issue("AUTH-123")
+@allure.testcase("TMS-456")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_5_is_prime():
     assert bl.is_prime(5) == True
 
+@allure.title("Test 7 is prime ")
+@allure.description("This test attempts to test wether 4 is prime or not.")
+@allure.tag("NewUI", "Essentials", "Authentication")
+@allure.label("owner", "John Doe")
+@allure.link("https://dev.example.com/", name="Website")
+@allure.issue("AUTH-123")
+@allure.testcase("TMS-456")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_7_is_prime():
-    assert bl.is_prime(7) == False
+    assert bl.is_prime(7) == True
